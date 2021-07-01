006c53d514a94cdca3975fcc73090fd90e9c3166
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2021-07-01 23:24:41 +1200

    Updated Script

--- DBScripts/01_Identity_Insert_Script.sql
+++ DBScripts/01_Identity_Insert_Script.sql
@@ -7,16 +7,17 @@ values(NEWID(),'Approver','APPROVER',NEWID());
 INSERT INTO [Identity].[User]
            ([Id],[UserName],[NormalizedUserName],[Email],[NormalizedEmail],[EmailConfirmed],[PasswordHash]
            ,[SecurityStamp],[ConcurrencyStamp],[PhoneNumber],[PhoneNumberConfirmed],[TwoFactorEnabled]
-           ,[LockoutEnd],[LockoutEnabled],[AccessFailedCount],[FirstName],[LastName])
+           ,[LockoutEnd],[LockoutEnabled],[AccessFailedCount])
 VALUES (NEWID(),'Creator','CREATOR','creator@gmail.com','CREATOR@GMAIL.COM',1,'AQAAAAEAACcQAAAAEE9fE6kGsB49SPaa/w+z23trvIHHPYIcgsIHGCJ4e571nNhg7Mmnj9q5/64Y3FUKSw==',
-'KFLB4TVZXNQRH67BB73ZSFLXS7HAKOMO','ab902cd6-8d33-4c47-aa6a-ae2247e0ba6e',null,0,0,null,1,0,'Creator','Creator');
+'KFLB4TVZXNQRH67BB73ZSFLXS7HAKOMO','ab902cd6-8d33-4c47-aa6a-ae2247e0ba6e',null,0,0,null,1,0);
+
 
 INSERT INTO [Identity].[User]
            ([Id],[UserName],[NormalizedUserName],[Email],[NormalizedEmail],[EmailConfirmed],[PasswordHash]
            ,[SecurityStamp],[ConcurrencyStamp],[PhoneNumber],[PhoneNumberConfirmed],[TwoFactorEnabled]
-           ,[LockoutEnd],[LockoutEnabled],[AccessFailedCount],[FirstName],[LastName])
+           ,[LockoutEnd],[LockoutEnabled],[AccessFailedCount])
 VALUES (NEWID(),'Approver','APPROVER','Approver@gmail.com','APPROVER@GMAIL.COM',1,'AQAAAAEAACcQAAAAEE9fE6kGsB49SPaa/w+z23trvIHHPYIcgsIHGCJ4e571nNhg7Mmnj9q5/64Y3FUKSw==',
-'KFLB4TVZXNQRH67BB73ZSFLXS7HAKOMO','ab902cd6-8d33-4c47-aa6a-ae2247e0ba6e',null,0,0,null,1,0,'Approver','Approver');
+'KFLB4TVZXNQRH67BB73ZSFLXS7HAKOMO','ab902cd6-8d33-4c47-aa6a-ae2247e0ba6e',null,0,0,null,1,0);
 
 
 INSERT INTO [Identity].[UserRoles]
